42155edc1109fd2b5e6b1c2420ec20d436384f7f
Pierre Lingat <pierre@helloworld.college>
2021-07-22 23:32:48 +1000

    Level order and post order prints added

--- Trees/bst.py
+++ Trees/bst.py
@@ -5,22 +5,68 @@ class TreeNode:
         self.right = None
     
     def add(self, data):
-        if(data > self.data and node.right == None):
-            node.right = TreeNode(data)
-        elif(data > self.data and node.right != None):
-            node.right.add(data)
-        if(data < self.data and node.left == None):
-            node.right = TreeNode(data)
-        elif(data < self.data and node.left != None):
-            node.right.add(data)
+        if(data > self.data and self.right == None):
+            self.right = TreeNode(data)
+        elif(data > self.data and self.right != None):
+            self.right.add(data)
+        if(data < self.data and self.left == None):
+            self.left = TreeNode(data)
+        elif(data < self.data and self.left != None):
+            self.left.add(data)
 
+
+def findMax(a, b):
+    if a >= b:
+      return a;
+    else:
+      return b;
+
+def height(root):
+    # Base case:
+    if root is None:
+        return 0;
+
+    return findMax(height(root.left), height(root.right)) + 1;
 
             
 
     
 def inOrderTransversal(node): 
-    if(node != null):
+    if(node != None):
         inOrderTransversal(node.left)
-        print(node)
+        print(node.data)
         inOrderTransversal(node.right)
+
+def printLevelOrder(node):
+    h = height(node)
+    spacing = h + 1
+    for i in range(1, h + 1):
+        print(" " * spacing, end = '')
+        printGivenLevel(node, i)
+        print()
+        spacing-=1
+
+def printGivenLevel(node, level):
+    if node is None:
+        print("  ", end = '')
+        return node
+     
+    if level == 1:
+        print(node.data, end = '  ')
+    elif level > 1:
+        printGivenLevel(node.left, level - 1)
+        printGivenLevel(node.right, level - 1)
         
+node = TreeNode(50)
+node.add(2)
+node.add(-1)
+node.add(10)
+node.add(5)
+node.add(20)
+node.add(2)
+node.add(60)
+node.add(65)
+
+# inOrderTransversal(node)
+
+printLevelOrder(node)
