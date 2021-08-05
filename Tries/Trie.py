f5025e8592e54b1534d42b8da7de8c951634b04a
Pierre Lingat <pierre@helloworld.college>
2021-08-05 18:05:35 +1000

    Created own way to display tries

---
+++ Tries/Trie.py
@@ -0,0 +1,80 @@
+'''
+TRIE SAMPLE
+Created by Pierre Lingat 
+Date: 05/08/21
+'''
+ALPHABET_SIZE = 26
+
+class Node:
+
+    # node for the trie
+    def __init__(self):
+        # there are 26 letters in the alphabet, duplicates shouldn't exist
+        self.children = [None]*26
+
+        # represents the end of the word, True if node is
+        self.isEnd = False
+
+
+class Trie:
+
+    # Trie data structure
+    def  __init__(self):
+        self.root = self.getNode()
+
+    def getNode(self):
+        return Node()
+
+    # this was found on geeksforgeeks 
+    def _charToIndex(self, ch):
+        # private helper function
+        # Converts key current character into index
+        # use only 'a' through 'z' and lower case
+         
+        return ord(ch)-ord('a')
+ 
+    def insert(self, word):
+
+        trie = self.root
+        wordLength = len(word)
+        for ch in range(wordLength):
+            index = self._charToIndex(word[ch])
+
+            if not trie.children[index]:
+                trie.children[index] = Node()
+            trie = trie.children[index]
+
+        trie.isEnd = True
+
+
+
+def displayTrie(trieNode, word, index):
+    if index != -1:
+        word = word + chr(ord('a') + index)
+    if(trieNode.isEnd):
+        print(word)
+        word = ""
+  
+    for child in range(len(trieNode.children)):
+        if(trieNode.children[child] != None):
+            displayTrie(trieNode.children[child], word, child)
+        
+
+
+def main():
+ 
+    # Input keys (use only 'a' through 'z' and lower case)
+    words = ["hello", "how", "alone", "biscuit"]
+ 
+ 
+    # Trie object
+    t = Trie()
+ 
+    # Construct trie
+    for word in words:
+        t.insert(word)
+
+    displayTrie(t.root, "", -1)
+ 
+if __name__ == '__main__':
+    main()
